3337e32af84c047d5bfd40f78619029e5dbc895c
like278016058 <75259457+like278016058@users.noreply.github.com>
2020-12-01 22:25:24 +0800

    Update IdeagithubApplicationTests.java
    第四次提交

--- src/test/java/com/derder/IdeagithubApplicationTests.java
+++ src/test/java/com/derder/IdeagithubApplicationTests.java
@@ -10,6 +10,7 @@ class IdeagithubApplicationTests {
 	void contextLoads() {
               System.out.println("是个derder");
 		System.out.println("你个老derder");
+System.out.println("你个老derder");
 	}
 
 }
