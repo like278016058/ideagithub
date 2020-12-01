7c9a0bc01481a888a22779e990eaa966d8e75fde
xiaohui <like278016058@163.com>
2020-12-01 22:03:21 +0800

    第一次提交

---
+++ src/main/java/com/derder/IdeagithubApplication.java
@@ -0,0 +1,13 @@
+package com.derder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeagithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeagithubApplication.class, args);
+	}
+
+}
