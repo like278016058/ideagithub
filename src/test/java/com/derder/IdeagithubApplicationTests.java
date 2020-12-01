875b6bb8fd2bda0049c40ec718b4d69d73ea2477
xiaohui <like278016058@163.com>
2020-12-01 22:40:21 +0800

    第7次提交

--- src/test/java/com/derder/IdeagithubApplicationTests.java
+++ src/test/java/com/derder/IdeagithubApplicationTests.java
@@ -8,7 +8,7 @@ class IdeagithubApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("是个derder");
+		System.out.println("是个derderwa");
 		System.out.println("你个老derder");
 		System.out.println("你个老derder");
 		System.out.println("你个老derder");
